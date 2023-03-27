0669744c8dc55c2f9a4c868705c07a0bb6e26790
Ville Koch <villekoch87@gmail.com>
2023-03-27 16:12:40 +0300

    added MS Graph App Role Assignment export

--- Export_AzureAD-RBAC-RoleAssignments.ps1
+++ Export_AzureAD-RBAC-RoleAssignments.ps1
@@ -2,6 +2,8 @@
 $date = Get-Date -Format 'dd_MM_yyyy-HH_mm_ss'
 $AzRoleAssignmentsCSV = "$env:USERPROFILE\Desktop\$date`_AzRoleAssignments.csv"
 $AzureADRoleAssignmentsCSV = "$env:USERPROFILE\Desktop\$date`_AzureADRoleAssignments.csv"
+$MSGraphUserAppRoleAssignmentsCSV = "$env:USERPROFILE\Desktop\$date`_MSGraphUserAppRoleAssignments.csv"
+$MSGraphSPAppRoleAssignmentsCSV = "$env:USERPROFILE\Desktop\$date`_MSGraphSPAppRoleAssignments.csv"
 
 # Connect to Azure using Az module...
 Write-Host "Connecting to Azure using Az module..."
@@ -33,7 +35,7 @@ Connect-AzureAD
 # Start main part of script #
 #############################
 
-# RBAC role assignments
+############# RBAC role assignments #############
 $AllAzRoleAssignmentsList = @()
 try {
     $AllAzRoleAssignments = Get-AzRoleAssignment -Scope $subScope 
@@ -70,7 +72,7 @@ catch {
     Write-Warning "There was an error when trying to get the Azure RBAC role assignments for the subscription $($SubToAudit)!"
 }
 
-# AzureAD role assignments
+############# AzureAD role assignments #############
 try{
     $UserRoles = Get-AzureADDirectoryRole | ForEach-Object {
         $Role = $_
@@ -93,4 +95,81 @@ try{
 }catch{
     Write-Warning "There was an error when trying to get the Azure AD role assignments!"
 }
-Write-Host "Script finished! You can find the csv files here: `r`n$AzRoleAssignmentsCSV`r`n$AzureADRoleAssignmentsCSV"
+
+############# MS Graph permissions #############
+Write-Host "Collecting app role assignments from MS Graph..."
+# Get all service principals
+$SPsURI = "$MSGraphURL/v1.0/servicePrincipals"
+$SPObjects = $null
+$ServicePrincipals = $null
+Write-Host "Collecting Service Principals in environment..."
+do{
+    $ServicePrincipals = Invoke-RestMethod `
+        -Headers $Headers `
+        -URI $SPsURI `
+        -UseBasicParsing `
+        -Method "GET" `
+        -ContentType "application/json"
+    if($ServicePrincipals.value){
+        $SPObjects += $ServicePrincipals.value 
+    }else{
+        $SPObjects += $ServicePrincipals
+    }
+    $SPsURI = $ServicePrincipals.'@odata.nextlink'
+} until (!($SPsURI))
+
+# Get all users
+$UsersURI = "$MSGraphURL/v1.0/users"
+$UsersObjects = $null	
+$Users = $null
+Write-Host "Collecting Users in environment..."
+do{
+    $Users = Invoke-RestMethod `
+        -Headers $Headers `
+        -URI $UsersURI `
+        -UseBasicParsing `
+        -Method "GET" `
+        -ContentType "application/json"
+    if($Users.value){
+        $UsersObjects += $Users.value 
+    }else{
+        $UsersObjects += $Users
+    }
+    $UsersURI = $Users.'@odata.nextlink'
+} until (!($UsersURI))
+
+# Get User AppRoleAssignments
+$UserIDs = ($UsersObjects).id
+$UserAppRoleAssignments = $null
+ForEach ($id in $UserIDs){
+    $req = $null
+    $UserAppRoleAssignmentURL = 'https://graph.microsoft.com/v1.0/users/{0}/appRoleAssignments' -f $id
+    $req = Invoke-RestMethod -Headers $Headers `
+        -Uri $UserAppRoleAssignmentURL `
+        -Method GET
+    $UserAppRoleAssignments += $req.value
+}
+if(($UserAppRoleAssignments | measure).Count -gt 0){
+    $UserAppRoleAssignments | Export-CSV -NoTypeInformation -Path $MSGraphUserAppRoleAssignmentsCSV
+}else{
+    "None identified" | Out-File -Path $MSGraphUserAppRoleAssignmentsCSV
+}
+
+# Get SP AppRoleAssignments
+$ServicePrincipalIDs = ($SPObjects).id
+$SPAppRoleAssignments = $null
+ForEach ($id in $ServicePrincipalIDs){
+    $req = $null
+    $SPAppRoleAssignmentURL = 'https://graph.microsoft.com/v1.0/servicePrincipals/{0}/appRoleAssignments' -f $id
+    $req = Invoke-RestMethod -Headers $Headers `
+        -Uri $SPAppRoleAssignmentURL `
+        -Method GET
+    $SPAppRoleAssignments += $req.value
+}
+if(($SPAppRoleAssignments | measure).Count -gt 0){
+    $SPAppRoleAssignments | Export-CSV -NoTypeInformation -Path $MSGraphSPAppRoleAssignmentsCSV
+}else{
+    "None identified" | Out-File -Path $MSGraphSPAppRoleAssignmentsCSV
+}
+
+Write-Host "Script finished! You can find the csv files here: `r`n$AzRoleAssignmentsCSV`r`n$AzureADRoleAssignmentsCSV`r`n$MSGraphUserAppRoleAssignmentsCSV`r`n$MSGraphSPAppRoleAssignmentsCSV"
