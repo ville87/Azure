df6a5e883cb95bdd6aca545a330895d8c440cfc7
Ville Koch <villekoch87@gmail.com>
2023-03-29 08:31:27 +0300

    some fixes

--- Export_AzureAD-RBAC-RoleAssignments.ps1
+++ Export_AzureAD-RBAC-RoleAssignments.ps1
@@ -1,5 +1,14 @@
 # Script to export Azure AD role assignments, RBAC role assignments from a chosen subscription and MS Graph app role assignments for users and Service Principals.
 [string]$MSGraphURL = "https://graph.microsoft.com"
+function Get-AzureGraphToken {
+    # Taken from https://gist.githubusercontent.com/andyrobbins/7c3dd62e6ed8678c97df9565ff3523fb/raw/2543368cc661820bc1d13e21aecab5f472086db2/AuditAppRoles.ps1
+    $APSUser = Get-AzContext *>&1 
+    $resource = "$MSGraphURL"
+    $Token = [Microsoft.Azure.Commands.Common.Authentication.AzureSession]::Instance.AuthenticationFactory.Authenticate($APSUser.Account, $APSUser.Environment, $APSUser.Tenant.Id.ToString(), $null, [Microsoft.Azure.Commands.Common.Authentication.ShowDialog]::Never, $null, $resource).AccessToken
+    $Headers = @{}
+    $Headers.Add("Authorization","Bearer"+ " " + "$($token)")
+    $Headers
+}
 
 # Exportfiles are defined here
 $date = Get-Date -Format 'dd_MM_yyyy-HH_mm_ss'
@@ -101,6 +110,7 @@ try{
 
 ############# MS Graph permissions #############
 Write-Host "Collecting app role assignments from MS Graph..."
+$Headers = Get-AzureGraphToken
 # Get all service principals
 $SPsURI = "$MSGraphURL/v1.0/servicePrincipals"
 $SPObjects = $null
