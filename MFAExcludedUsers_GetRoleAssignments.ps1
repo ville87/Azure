b26b603e064c66849436d513ef27158d04ea87a0
ville87 <38942588+ville87@users.noreply.github.com>
2023-08-10 09:47:51 +0300

    added audit of azure rbac roles

--- MFAExcludedUsers_GetRoleAssignments.ps1
+++ MFAExcludedUsers_GetRoleAssignments.ps1
@@ -1,5 +1,7 @@
 $tenantID               = Read-Host "Please provide the tenantId of the target Azure AD tenant"
 $MSGraphURL             = "https://graph.microsoft.com"
+$AzRMURL                = "https://management.azure.com"
+$CSVPath                = "$((Get-Location).Path)\MFAExcludedUsers_Roles_$tenantId`_$(Get-Date -Format 'dd_MM_yyyy-HH_mm_ss').csv"
 function Get-AzureGraphToken {
     $APSUser = Get-AzContext *>&1 
     $resource = "$MSGraphURL"
@@ -9,6 +11,23 @@ function Get-AzureGraphToken {
     $Headers.Add("Content-Type","application/json")
     $Headers
 }
+function Get-AzureRMToken {
+    $azContext = Get-AzContext
+    $azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+    $profileClient = New-Object -TypeName Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient -ArgumentList ($azProfile)
+    if($null -eq $azContext.Subscription.TenantId){
+        # Had to add this for environments without Azure Subscription (M365 only)
+        $token = $profileClient.AcquireAccessToken($azContext.Tenant.Id)
+    }else{
+        $token = $profileClient.AcquireAccessToken($azContext.Subscription.TenantId)
+    }
+    $authHeader = @{
+        'Content-Type'='application/json'
+        'Authorization'='Bearer ' + $token.AccessToken
+    }
+    $authHeader
+}
+
 try{
     Write-Host "Importing necessary modules..."
     Import-Module Az.Accounts -ErrorAction Stop
@@ -29,7 +48,13 @@ $MGGraphBetaHeaders.Add("ConsistencyLevel","eventual")
 $FileofMFAExcludedUsers = Read-Host "Please provide the full file path to the textfile containing the list of MFA excluded users. The users have to be added in their upn format, e.g. user@domain.com, one user per line"
 $FileofMFAExcludedUsers = $FileofMFAExcludedUsers -replace ('"','')
 $ListofMFAExcludedUsers = get-Content $FileofMFAExcludedUsers
-$ResultArrayList =  New-Object System.Collections.ArrayList
+$RolesList =  New-Object System.Collections.ArrayList
+# Get all subscriptions where current logged in user has access to, because we can only audit RBAC role assignments in these...
+$authHeader = Get-AzureRMToken
+$restUri = "$AzRMURL/subscriptions/?api-version=2020-01-01"
+$subscriptions = Invoke-RestMethod -Uri $restUri -Method Get -Headers $authHeader
+$subscriptionsobj = $subscriptions.value
+
 foreach($MFAExcludedUser in $ListofMFAExcludedUsers){
     try {
         $null = [mailaddress]$MFAExcludedUser
@@ -38,27 +63,57 @@ foreach($MFAExcludedUser in $ListofMFAExcludedUsers){
         Write-Warning "The current user $MFAExcludedUser is not in the correct format for a user principal name! Will skip the check for this one..."
         continue
     }
-    Write-Host "Collecting assigned roles for user $MFAExcludedUser..."
+    
+    ###############################################################################################
+    ################################# Azure AD Role Assignments ###################################
+    ###############################################################################################
+
+    Write-Host "Collecting assigned Azure AD roles for user $MFAExcludedUser..."
     $UserId = (Get-AzADUser -UserPrincipalName $MFAExcludedUser).Id
-    $RoleAssignmentsURL = "$MSGraphURL/beta/rolemanagement/directory/transitiveRoleAssignments?`$count=true&`$filter=principalId eq '$UserId'"
-    $AzureADRoleAssignmentResponse = Invoke-RestMethod -Headers $MGGraphBetaHeaders -URI $RoleAssignmentsURL -UseBasicParsing
-    $AssignedRoles = $AzureADRoleAssignmentResponse.value
-    if($AssignedRoles.count -gt 0){
+    $AzureADRoleAssignmentsURL = "$MSGraphURL/beta/rolemanagement/directory/transitiveRoleAssignments?`$count=true&`$filter=principalId eq '$UserId'"
+    $AzureADRoleAssignmentResponse = Invoke-RestMethod -Headers $MGGraphBetaHeaders -URI $AzureADRoleAssignmentsURL -UseBasicParsing
+    $AssignedAzureADRoles = $AzureADRoleAssignmentResponse.value
+    if($AssignedAzureADRoles.count -gt 0){
         # Lookup role definition names
-        foreach($AssignedRole in $AssignedRoles){
-            $RoleDefinitionsResponse = Invoke-RestMethod -Uri "https://graph.microsoft.com/v1.0/roleManagement/directory/roleDefinitions?`$filter=Id eq '$($AssignedRole.roleDefinitionId)'" -Headers $Headers
-            $data = [pscustomobject]@{'UserPrincipalName'=$MFAExcludedUser;'RoleDisplayName'=$RoleDefinitionsResponse.value.displayName;}
-            $ResultArrayList.add($data) | Out-Null
+        foreach($AssignedAzureADRole in $AssignedAzureADRoles){
+            $RoleDefinitionsResponse = Invoke-RestMethod -Uri "https://graph.microsoft.com/v1.0/roleManagement/directory/roleDefinitions?`$filter=Id eq '$($AssignedAzureADRole.roleDefinitionId)'" -Headers $Headers
+            $data = [pscustomobject]@{'UserPrincipalName'=$MFAExcludedUser;'RoleType'='AzureAD';'RoleDisplayName'=$RoleDefinitionsResponse.value.displayName;'Scope'='N/A';}
+            $RolesList.add($data) | Out-Null
         }
     }else{
-        Write-Host "User has no roles assigned."
-        $data = [pscustomobject]@{'UserPrincipalName'=$MFAExcludedUser;'RoleDisplayName'="N/A"}
-        $ResultArrayList.add($data) | Out-Null
+        $data = [pscustomobject]@{'UserPrincipalName'=$MFAExcludedUser;'RoleType'='AzureAD';'RoleDisplayName'="None identified";'Scope'='N/A';}
+        $RolesList.add($data) | Out-Null
 
+    }
+
+    ###############################################################################################
+    ################################# Azure RBAC Role Assignments #################################
+    ###############################################################################################
+    Write-Host "Collecting assigned Azure RBAC roles for user $MFAExcludedUser..."
+    # This has to be done for every subscription we have access to
+    foreach($subscription in $subscriptionsobj){
+        $AzRBACRoleAssRestUri = "$AzRMURL/subscriptions/$($subscription.subscriptionId)/providers/Microsoft.Authorization/roleAssignments?api-version=2022-04-01&`$filter=assignedTo('$userId')"
+        $AzRBACRoleAssignmentResponse = Invoke-RestMethod -Uri $AzRBACRoleAssRestUri -Method Get -Headers $authHeader
+        if(($AzRBACRoleAssignmentResponse.value.properties).count -gt 0){
+            $UsersAzRBACRoleAssignments = $AzRBACRoleAssignmentResponse.value.properties
+            foreach($UsersAzRBACRoleAssignment in $UsersAzRBACRoleAssignments){
+                # Get roleDefName from roleDefId
+                $UsersAzRBACRoleAssignmentroleDefinitionId = $UsersAzRBACRoleAssignment.roleDefinitionId
+                $roleDefIdURL = "$AzRMURL/$UsersAzRBACRoleAssignmentroleDefinitionId`?api-version=2022-04-01"
+                $roleDefResponse = Invoke-RestMethod -Uri $roleDefIdURL -Method Get -Headers $authHeader
+                $roleDisplayName = $roleDefResponse.properties.roleName
+                $data = [pscustomobject]@{'UserPrincipalName'=$MFAExcludedUser;'RoleType'='AzureRBAC';'RoleDisplayName'=$roleDisplayName;'Scope'=$($UsersAzRBACRoleAssignment.scope);}
+                $RolesList.add($data) | Out-Null
+            }
+        }else{
+            $data = [pscustomobject]@{'UserPrincipalName'=$MFAExcludedUser;'RoleType'='AzureRBAC';'RoleDisplayName'='None identified';'Scope'='N/A';}
+            $RolesList.add($data) | Out-Null
+        }
     }
 }
 
 Write-Host "Data collection done! Exporting the CSV file..."
-$CSVPath = "$((Get-Location).Path)\MFAExcludedUsers_RoleAssignments_$tenantId`_$(Get-Date -Format 'dd_MM_yyyy-HH_mm_ss').csv"
-$ResultArrayList | Export-Csv -Notypeinformation -Path $CSVPath -Force
-Write-Host "File exported to $CSVPath. Script finished!"
+$RolesList | Export-Csv -Notypeinformation -Path $CSVPath -Force
+Write-Host "File exported to $CSVPath"
+Write-Host "Script finished!`r`nPlease note that the logged in user had access to the following subscription(s), therefore the assigned RBAC roles could only be checked for those:"
+$subscriptionsobj | Select-Object subscriptionId,tenantId,displayName
