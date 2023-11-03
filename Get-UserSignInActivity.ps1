1782d72448f9720d9b03577c057c9288492c48d7
Ville Koch <villekoch87@gmail.com>
2023-11-03 11:13:18 +0200

    updated signinactivity script

--- Get-UserSignInActivity.ps1
+++ Get-UserSignInActivity.ps1
@@ -1,4 +1,4 @@
-# Script to get user signin activity data from MS Graph API
+# Script to get last user signin activity data from MS Graph API
 ########### Vars and helper functions ########### 
 [string]$MSGraphURL = "https://graph.microsoft.com"
 function Get-AzureGraphToken {
@@ -12,11 +12,23 @@ function Get-AzureGraphToken {
 }
 
 ########### MAIN ########### 
+################### check necessary module ###################
+printInfo -info "Checking if required PowerShell modules are available..." -level "INFO"
+try{
+    Import-Module Az.Accounts -ErrorAction Stop
+    Import-Module Az.Resources -ErrorAction Stop
+}catch{
+    Write-Warning "Required modules could not be imported! Please ensure you have the modules Az.Accounts and Az.Resources installed and restart the script!"
+    write-host $Error[0].Exception.Message
+    Exit 2
+}
+########## Login to tenant ########## 
 $tenantId = Read-host "Please provide the tenant Id"
 $ConnectAzAcc = Connect-AzAccount -TenantID $tenantId
 $Headers = Get-AzureGraphToken
+########## Collect data ########## 
 $AllUsersData = @()
-$UsersURL = "$MSGraphURL/v1.0/users/?`$select=userPrincipalName,signInActivity,lastPasswordChangeDateTime&`$top=999"
+$UsersURL = "$MSGraphURL/v1.0/users/?`$select=userPrincipalName,signInActivity&`$top=999"
 # Add userdata to array
 do{
     $UsersResponse = Invoke-RestMethod -Headers $Headers -URI $UsersURL -UseBasicParsing -Method "GET"
@@ -25,5 +37,5 @@ do{
     }
     $UsersURL = $UsersResponse.'@odata.nextlink'
 } until (!($UsersURL))
-Write-Host "Found $($AllUsersData.count) users:"
-$AllUsersData
+Write-Host "Found $($AllUsersData.count) users.`r`nExporting JSON file to $($env:userprofile)\Desktop\MSGraphUsersignInActivity.json"
+$AllUsersData | ConvertTo-Json | Out-File "$env:userprofile\Desktop\MSGraphUsersignInActivity.json"
