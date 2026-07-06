a7af7d403c7feb8545dee88b7e3f95b8be3cfd15
ville87 <38942588+ville87@users.noreply.github.com>
2026-07-06 08:08:41 +0300

    Refactor script to use Microsoft Graph API
    Updated the script to use the Microsoft Graph module for assigning permissions instead of Azure AD module.

--- Assign-MSGraphAPI-Permissions.ps1
+++ Assign-MSGraphAPI-Permissions.ps1
@@ -1,24 +1,3 @@
-# Script to add MS Graph API Permissions to a managed identity
-
-# define the service principal
-$mgdidentity = Get-AzureADServicePrincipal -ObjectId d7b92146-bebe-46b7-b762-d2d6a46d0b43
-
-# permission name (note: the following one is a very high privileged permission!)
-$permissionname ="RoleManagement.ReadWrite.Directory"
-
-# MS Graph App Id (This is always the same!)
-$GraphAppId = "00000003-0000-0000-c000-000000000000"
-
-# Get the Graph SP
-$GraphServicePrincipal = Get-AzureADServicePrincipal -Filter "appId eq '$GraphAppId'"
-
-# Get the user defined graph app role
-$AppRole = $GraphServicePrincipal.AppRoles | Where-Object {$_.Value -eq $PermissionName -and $_.AllowedMemberTypes -contains "Application"}
-
-# assign the permission
-New-AzureAdServiceAppRoleAssignment -ObjectId $mgdidentity.ObjectId -PrincipalId $mgdidentity.ObjectId -ResourceId $GraphServicePrincipal.ObjectId -Id $AppRole.Id
-
-#########################################
 # Using the MG module (from: https://stackoverflow.com/a/72905062)
 
 $DestinationTenantId = Read-Host "please provide the tenant Id"
@@ -34,12 +13,11 @@ $oPermissions = @(
 )
 
 $GraphAppId = "00000003-0000-0000-c000-000000000000" # Don't change this.
+Connect-MgGraph -TenantId $DestinationTenantId
 
 $oGraphSpn = Get-MgServicePrincipal -Filter "appId eq '$GraphAppId'"
 
 $oAppRole = $oGraphSpn.AppRoles | Where-Object {($_.Value -in $oPermissions) -and ($_.AllowedMemberTypes -contains "Application")}
-
-Connect-MgGraph -TenantId $DestinationTenantId
 
 foreach($AppRole in $oAppRole)
 {
